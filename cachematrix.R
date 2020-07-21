e1f82017de42672a67d299c06f0b54d39c5d67fc
Carol-Coutinho <68571040+Carol-Coutinho@users.noreply.github.com>
2020-07-21 15:41:17 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Carol_Coutinho
+#Function Assignment2
 
-## Write a short comment describing this function
+#First function calculate the determinant of the matrix.
+#If the Det is different of zero, the your matrix assume inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  Det <- det(x)
+  if (Det != 0){
+    print("Your matrix can assume inverse, you can use cacheSolve function.")
+  }
+  else {
+    print("Please, choose another matrix")
+  }
 }
 
+#Please input your matrix here:
+
+x <-  #matrix(c(5, 1, 0,
+        #3,-1, 2,
+        #4, 0,-1), nrow=3, byrow=TRUE) #Exemple
+
+makeCacheMatrix(x)
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ inverse <-  solve(x)%*%x
 }
+
+y <- cacheSolve(x)
